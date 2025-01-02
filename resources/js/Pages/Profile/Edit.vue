17564bb64c4c9596ac6c1ca2b358764818a49ea4
Vun Kian Hiung <hiung2009@live.com>
2025-01-02 22:19:02 +0800

    Day 5.3 - Profile update/Email verify once edited

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -1,9 +1,14 @@
 <script setup>
+import UpdateInfo from './Sections/UpdateInfo.vue';
 
+defineProps({
+    user: Object,
+    status: String,
+});
 </script>
 
 <template>
-    <div>
-        Profile
-    </div>
+
+    <Head title="- Profile" />
+    <UpdateInfo :user="user" :status="status"/>
 </template>
