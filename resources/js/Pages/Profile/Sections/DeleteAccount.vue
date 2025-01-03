8ea370c46b90a9e4c2771b7fd39add604e832e3a
Vun Kian Hiung <hiung2009@live.com>
2025-01-03 15:39:35 +0800

    Day 6.2

--- resources/js/Pages/Profile/Sections/DeleteAccount.vue
+++ resources/js/Pages/Profile/Sections/DeleteAccount.vue
@@ -5,11 +5,21 @@ import Title from '../../../Components/Title.vue';
 import PrimaryBtn from '../../../Components/PrimaryBtn.vue';
 import ErrorMessages from '../../../Components/ErrorMessages.vue';
 import { useForm } from '@inertiajs/vue3';
-import { space } from 'postcss/lib/list';
+import { ref } from 'vue';
+
+const showConfirmPassword = ref(false);
 
 const form = useForm({
     password: '',
 });
+
+const submit = () => {
+    form.delete(route("profile.destroy"), {
+        onSuccess: () => form.reset(),
+        onError: () => form.reset(),
+        preserveScroll: true,
+    });
+};
 
 </script>
 
@@ -23,13 +33,22 @@ const form = useForm({
 
         <ErrorMessages :errors="form.errors" />
 
-        <form @submit.prevent="" class="space-y-6">
-            <InputField label="Confirm Password" icon="key" type="password" class="w-1/2" v-model="form.password" />
+        <div v-if="showConfirmPassword">
+            <form @submit.prevent="submit" class="space-y-6">
+                <InputField label="Confirm Password" icon="key" type="password" class="w-1/2" v-model="form.password" />
 
-            <div class="flex space-x-4">
-                <PrimaryBtn :diabled="form.processing">Confirm</PrimaryBtn>
-                <button>Cancel</button>
-            </div>
-        </form>
+                <div class="flex space-x-4">
+                    <PrimaryBtn :diabled="form.processing">Confirm</PrimaryBtn>
+                    <button @click="showConfirmPassword = false"
+                        class="text-indigo-500 font-medium underline dark:text-indigo-400">Cancel</button>
+                </div>
+            </form>
+        </div>
+
+        <button v-if="!showConfirmPassword" @click="showConfirmPassword = true"
+            class="px-6 py-2 rounded-lg bg-red-500 text-white my-6">
+            <i class="fa-solid fa-triangle-exclamation mr-2"></i>
+            Delete Account
+        </button>
     </Container>
 </template>
