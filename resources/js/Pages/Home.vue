5c5dfb794c5373fbdc0442bec0e7da6685cf903c
Vun Kian Hiung <hiung2009@live.com>
2025-01-08 21:57:05 +0800

    Day 9.0 - Search engine on home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,17 +1,37 @@
 <script setup>
 import Card from '../Components/Card.vue';
 import PaginationLinks from '../Components/PaginationLinks.vue';
+import InputField from '../Components/InputField.vue';
+import { router, useForm } from '@inertiajs/vue3';
 
 defineProps({
     listings: Object,
+});
 
-})
+const form = useForm({
+    search: '',
+});
+
+const search = () => {
+    router.get(route('home'), { search: form.search });
+};
 </script>
 
 <template>
-    {{ console.log(listings) }}
-
     <Head title="- Latest Listings" />
+
+    <div class="flex items-center justify-between mb-4">
+        <div>
+            filters
+        </div>
+
+        <div class="w-1/4">
+            <form action="">
+                <InputField type="search" label="" icon="magnifying-glass" placeholder="Search..." v-model="form.search"/>
+            </form>
+
+        </div>
+    </div>
 
     <div v-if="Object.keys(listings.data).length">
         <div class="grid grid-cols-3 gap-4">
@@ -22,5 +42,8 @@ defineProps({
         <div class="mt-8">
             <PaginationLinks :paginator="listings" />
         </div>
+    </div>
+    <div v-else>
+        There are no listings
     </div>
 </template>
